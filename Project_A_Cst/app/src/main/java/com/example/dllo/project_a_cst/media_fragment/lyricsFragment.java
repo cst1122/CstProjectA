4b2a34c9398fce7c06876bf9f4196e3b7eebf24a
ChiSongTing <748233223@qq.com>
2016-11-29 16:27:38 +0800

    音乐播放页面Activity中ViewPager的三个Fragment

---
+++ Project_A_Cst/app/src/main/java/com/example/dllo/project_a_cst/media_fragment/lyricsFragment.java
@@ -0,0 +1,26 @@
+package com.example.dllo.project_a_cst.media_fragment;
+
+import android.view.View;
+
+import com.example.dllo.project_a_cst.main_activity_fragment.BaseFragment;
+
+/**
+ * Created by dllo on 16/11/29.
+ */
+
+public class lyricsFragment extends BaseFragment{
+    @Override
+    public int setlayout() {
+        return 0;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
